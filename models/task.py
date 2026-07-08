95fac71da7341691304b6e7330437c7d7b54c356
99Csui <99csui@gmail.com>
2026-07-08 00:34:31 -0400

    feat: add task domain validation

--- models/task.py
+++ models/task.py
@@ -11,15 +11,64 @@ class Task:
     status: TaskStatus = TaskStatus.WAITING
     priority: TaskPriority = TaskPriority.MEDIUM
 
-    def change_status(self, new_status):
+    def __post_init__(self):
+        self._validate_id()
+        self._validate_title()
+        self._validate_description()
+        self._validate_status(self.status)
+        self._validate_priority(self.priority)
+
+
+    def change_status(self, new_status: TaskStatus) -> None:
+        self._validate_status(new_status)
         self.status = new_status
+        
 
 
-    def change_priority(self,new_priority):
+    def change_priority(self,new_priority: TaskPriority) -> None:
+        self._validate_priority(new_priority)
         self.priority = new_priority
+        
 
 
-    def mark_as_finished(self):
+    def mark_as_finished(self) -> None:
         self.change_status(TaskStatus.FINISHED)
 
 
+    def _validate_id(self) -> None:
+        if not isinstance(self.id,int):
+            raise TypeError("the id must be an int")
+        
+        if self.id < 1:
+            raise ValueError("id cannot be 0 or below")
+
+
+
+    def _validate_title(self) -> None:
+        if not isinstance(self.title,str):
+            raise TypeError("the title must be a string")
+        
+        if self.title.strip() == "":
+            raise ValueError("title cannot be empty")
+
+
+    def _validate_description(self) -> None:
+        if not isinstance(self.description,str):
+            raise TypeError("the description must be a string")
+        
+        if self.description.strip() == "":
+            raise ValueError("description cannot be empty")
+
+
+    def _validate_status(self, status: TaskStatus) -> None:
+        if not isinstance(status, TaskStatus):
+            raise TypeError("status must be a TaskStatus")
+
+
+    def _validate_priority(self, priority: TaskPriority) -> None:
+        if not isinstance(priority, TaskPriority):
+            raise TypeError("priority must be a TaskPriority")
+
+
+    
+    
--- tests/test_task.py
+++ tests/test_task.py
@@ -6,7 +6,7 @@ from models.task import Task
 class TestTask(unittest.TestCase):
 
     def test_task_start_with_default_status(self):
-        task = Task(1, "Study python", "Practice dataclasses")
+        task = Task(1, "Study Python", "Practice dataclasses")
 
         self.assertEqual(task.status, TaskStatus.WAITING)
 
@@ -17,14 +17,12 @@ class TestTask(unittest.TestCase):
         self.assertEqual(task.priority, TaskPriority.MEDIUM)
 
 
-
     def test_change_status_updates_status(self):
         task = Task(1, "Study Python", "Practice dataclasses")
         
         task.change_status(TaskStatus.IN_PROGRESS)
 
         self.assertEqual(task.status, TaskStatus.IN_PROGRESS)
-
 
 
     def test_change_priority_updates_priority(self):
@@ -35,13 +33,50 @@ class TestTask(unittest.TestCase):
         self.assertEqual(task.priority, TaskPriority.HIGH)
 
 
-
     def test_mark_as_finished_sets_status_to_finished(self):
         task = Task(1, "Study Python", "Practice dataclasses")
         
         task.mark_as_finished()
 
         self.assertEqual(task.status, TaskStatus.FINISHED)
+
+
+    def test_task_raises_type_error_when_title_is_not_string(self):
+        with self.assertRaises(TypeError):
+            Task(1, 1, "Practice dataclasses")
+
+
+    def test_task_raises_value_error_when_title_is_empty(self):
+        with self.assertRaises(ValueError):
+            task = Task(1, "", "Practice dataclasses")
+
+
+    def test_task_raises_value_error_when_title_has_only_spaces(self):
+        with self.assertRaises(ValueError):
+            task = Task(1, "     ", "Practice dataclasses")
+
+
+    def test_task_raises_type_error_when_status_is_invalid(self):
+        with self.assertRaises(TypeError):
+            task = Task(1, "Study Python", "Practice dataclasses", "banana")
+
+
+
+    def test_task_raises_type_error_when_priority_is_invalid(self):
+        with self.assertRaises(TypeError):
+            task = Task(1, "Study Python", "Practice dataclasses", TaskStatus.IN_PROGRESS, "banana")
+
+
+    def test_change_status_raises_type_error_when_status_is_invalid(self):
+        task = Task(1, "Study Python", "Practice dataclasses")
+        with self.assertRaises(TypeError):
+            task.change_status("banana")
+
+
+    def test_change_priority_raises_type_error_when_priority_is_invalid(self):
+        task = Task(1, "Study Python", "Practice dataclasses")
+        with self.assertRaises(TypeError):
+            task.change_priority("banana")
 
 
 
